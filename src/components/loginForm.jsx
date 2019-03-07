e9e2acc1309a0378c1fa85159a5abc6850a42831
paras1998 <pompolompo2@gmail.com>
2019-03-07 22:30:06 +0530

    VId 120 In this just added a simple logic for validation if input fileds are empty or not for this used .trim() over the input string obj to remove the space in suffix and prefix of the string as well as leaned about Object.keys(object name) it is used to return array of all key value pair whose values are strings

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -6,13 +6,21 @@ class LoginForm extends Component {
     error: {}
   };
   validate = () => {
-    return { username: "User name is req" };
+    const error = {};
+    const { account } = this.state;
+    if (account.username.trim() === "")
+      error.username = "Name can not be empty";
+    if (account.password.trim() === "")
+      error.password = "Password can not be empty";
+    return Object.keys(error).length === 0 ? null : error;
   };
   handleSubmit = e => {
     e.preventDefault();
     //call to validate function and further execution based on value of validate function return value
     const error = this.validate();
+    console.log(error);
     this.setState({ error });
+    console.log(Object.keys(this.state.error));
     if (error) return;
 
     //call server save change and redirect the user
